9150459ea3c60055f8b49216c628fab74cf2ee33
Hafsa.Malik <hafsa.malik@enigmatixglobal.com>
2024-12-17 15:17:56 +0500

    complete a fastapi pizza delivery project

---
+++ schemas.py
@@ -0,0 +1,65 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class SignUpModel(BaseModel):
+    id:Optional[int]
+    username:str
+    email:str
+    password:str
+    is_staff:Optional[bool]
+    is_active:Optional[bool]
+
+
+    class Config:
+        orm_mode=True
+        schema_extra={
+            'example':{
+                "username":"johndoe",
+                "email":"johndoe@gmail.com",
+                "password":"password",
+                "is_staff":False,
+                "is_active":True
+            }
+        }
+
+
+
+class Settings(BaseModel):
+    authjwt_secret_key:str='46b243b7f66e082ee0999169ea94861e9eb90ebbf2db54e2748b718bdc3085e9'
+
+
+class LoginModel(BaseModel):
+    username:str
+    password:str
+
+
+
+class OrderModel(BaseModel):
+    id:Optional[int]
+    quantity:int
+    order_status:Optional[str]="PENDING"
+    pizza_size:Optional[str]="SMALL"
+    user_id:Optional[int]
+
+
+    class Config:
+        orm_mode=True
+        schema_extra={
+            "example":{
+                "quantity":2,
+                "pizza_size":"LARGE"
+            }
+        }
+
+
+class OrderStatusModel(BaseModel):
+    order_status:Optional[str]="PENDING"
+
+    class Config:
+        orm_mode=True
+        schema_extra={
+            "example":{
+                "order_status":"PENDING"
+            }
+        }
